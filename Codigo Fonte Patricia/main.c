c21175ecf41dc211570083511702096bc5734e2d
Roniel Nunes <46944438+RonielNunes@users.noreply.github.com>
2020-10-10 22:28:04 -0300

    Inicio da patricia code ziviani

---
+++ Codigo Fonte Patricia/main.c
@@ -0,0 +1,28 @@
+/*
+    Grupo: Return NULL;
+
+    Eduardo Vinícius Bittencourt Esquivel 3498
+    Emamanuel Vitor Carvalho Ruela 3891
+    Roniel Nunes Barbosa 3464
+    Vinícius Tadeu Silva Ribeiro 2670
+*/
+#include "tad.h"
+
+int main(int argc, char *argv[]){ 
+    TipoArvore a = NULL;
+    TipoChave c;
+
+    int  i, j, k, n;
+    int  min = 32, max = 126;
+    TipoChave vetor[95];
+
+
+
+    FILE *arquivoEntrada = NULL;
+
+
+
+        
+    /* Gera uma permutacao aleatoria de chaves dos caracteres ASCII 32 a  126 */
+    return 0;
+} 
